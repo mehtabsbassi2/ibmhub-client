febf75513f5ad00b56c8ec746fc3fe98d789de4a
mehtabsbassi2 <148270493+mehtabsbassi2@users.noreply.github.com>
2025-07-25 19:31:56 +0100

    Update api.jsx

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //const BASE_URL = "http://localhost:5000/api"
-const BASE_URL="https://ibmhub-backend.vercel.app/api"
+const BASE_URL="https://ibmhub-backend.onrender.com/api"
 
 const api = axios.create({
     baseURL:BASE_URL,
