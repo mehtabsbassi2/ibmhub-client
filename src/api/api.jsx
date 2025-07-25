97508f52562aac856747f0dbcea7c3c1f29fb17b
mehtabsbassi2 <148270493+mehtabsbassi2@users.noreply.github.com>
2025-07-25 11:52:26 +0100

    Update api.jsx backend url connection for deployment

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
- const BASE_URL = "http://localhost:5000/api"
-//const BASE_URL="https://ibm-hub-backend.vercel.app/api"
+//const BASE_URL = "http://localhost:5000/api"
+const BASE_URL="https://ibmhub-backend.vercel.app/api"
 
 const api = axios.create({
     baseURL:BASE_URL,
