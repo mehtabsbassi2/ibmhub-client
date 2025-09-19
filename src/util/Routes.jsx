83cf60804359aa80e734710b46b019959260a09a
mehtabsbassi2 <148270493+mehtabsbassi2@users.noreply.github.com>
2025-09-19 14:59:26 +0300

    Update Routes.jsx

--- src/util/Routes.jsx
+++ src/util/Routes.jsx
@@ -6,4 +6,7 @@ export const SIGN_UP_PAGE = "/sign-up"
 export const UPDATE_PROFILE ="/update-profile"
 export const PROFILE = "/profile"
 export const DRAFTS = "/drafts"
+
 export const USER_QUESTIONS = "/user-questions"
+export const MANAGER = "/users/admin"
+export const USER_OVERVIEW ="/users/admin/:id"
