7430ada3f099c0b530d0ccc32323087c945554fb
mehtabsbassi2 <148270493+mehtabsbassi2@users.noreply.github.com>
2025-09-21 00:14:55 +0300

    Update QuestionDrafts.jsx

--- src/pages/drafts/QuestionDrafts.jsx
+++ src/pages/drafts/QuestionDrafts.jsx
@@ -1,70 +1,82 @@
-import React,{useState,useEffect} from 'react'
-import { useSelector,useDispatch } from 'react-redux';
-import { getProfile } from '../../redux/userSlice';
-import { getUserDraftQuestions } from '../../api/api';
-import { getDraftQuestions, setDraftQuestions } from '../../redux/questionSlice';
-import QuestionCard from '../questions/QuestionCard.';
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getProfile } from "../../redux/userSlice";
+import { getUserDraftQuestions } from "../../api/api";
+import {
+  getDraftQuestions,
+  setDraftQuestions,
+} from "../../redux/questionSlice";
+import QuestionCard from "../questions/QuestionCard.";
 
 const QuestionDrafts = () => {
-    const [search, setSearch] = useState("");
-      const [difficulty, setDifficulty] = useState("");
-      const [tag, setTag] = useState("");
-      const [status, setStatus] = useState("all");
-      const [userSkills, setUserSkills] = useState([]);
-    
-      const [sortBy, setSortBy] = useState("newest");
-       const [currentPage, setCurrentPage] = useState(1);
-        const [pagination, setPagination] = useState(null);
-        
-      
-        const profile = useSelector(getProfile)
-        const dispatch = useDispatch()
-          const quizes = useSelector(getDraftQuestions);
-        
+  const [search, setSearch] = useState("");
+  const [difficulty, setDifficulty] = useState("");
+  const [tag, setTag] = useState("");
+  const [status, setStatus] = useState("all");
+  const [userSkills, setUserSkills] = useState([]);
 
-      useEffect(()=>{
-        const fetchQuestionByUserSkills = async () =>{
-          try {
-            const queryParams = {
-                
-                  search,
-                  difficulty,
-                  tag,
-                  is_solved: status === "all" ? undefined : status === "solved" ? "true" : "false",
-                  sort: sortBy,
-                   page:currentPage
-                
-              };
-            const res = await getUserDraftQuestions(profile.id,queryParams)
-            dispatch(setDraftQuestions(res.data.questions || []))
-            setPagination(res.data.pagination);
-      
-            console.log("RES",res)
-      
-          } catch (error) {
-            
-          }
-        }
-        fetchQuestionByUserSkills()
-      },[profile?.id, search, difficulty, tag, status, sortBy, currentPage])
+  const [sortBy, setSortBy] = useState("newest");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pagination, setPagination] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const profile = useSelector(getProfile);
+  const dispatch = useDispatch();
+  const quizes = useSelector(getDraftQuestions);
+
+  useEffect(() => {
+    const fetchQuestionByUserSkills = async () => {
+      try {
+        setLoading(true);
+        const queryParams = {
+          search,
+          difficulty,
+          tag,
+          is_solved:
+            status === "all"
+              ? undefined
+              : status === "solved"
+              ? "true"
+              : "false",
+          sort: sortBy,
+          page: currentPage,
+        };
+        const res = await getUserDraftQuestions(profile.id, queryParams);
+        dispatch(setDraftQuestions(res.data.questions || []));
+        setPagination(res.data.pagination);
+
+        console.log("RES", res);
+      } catch (error) {
+        setLoading(false);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchQuestionByUserSkills();
+  }, [profile?.id, search, difficulty, tag, status, sortBy, currentPage]);
   return (
     <div>
-         {/* Cards */}
-      <div className=" w-[calc(100vw-350px)] space-y-4">
-
-        {Array.isArray(quizes) && quizes.length > 0 ? (
-          quizes.map((q) => <QuestionCard key={q.id} question={q} />)
-        ) : (
-          <div className="text-center text-gray-500 mt-10">
-            <p className="text-lg">No questions found.</p>
-            <p className="text-sm">
-              Try adjusting your filters or come back later.
-            </p>
-          </div>
-        )}
-      </div>
+      {/* Cards */}
+      {loading ? (
+        <div className="flex justify-center p-6">
+          <span className="loading loading-bars loading-xl text-ibmblue"></span>
+        </div>
+      ) : (
+        <div className=" w-[calc(100vw-350px)] space-y-4">
+          {Array.isArray(quizes) && quizes.length > 0 ? (
+            quizes.map((q) => <QuestionCard key={q.id} question={q} />)
+          ) : (
+            <div className="text-center text-gray-500 mt-10">
+              <p className="text-lg">No questions found.</p>
+              <p className="text-sm">
+                Try adjusting your filters or come back later.
+              </p>
+            </div>
+          )}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default QuestionDrafts
+export default QuestionDrafts;
