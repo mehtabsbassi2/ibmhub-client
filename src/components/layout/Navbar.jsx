20517b34ce08949f2ee860c845fc8f7b14e3164e
mehtabsbassi2 <148270493+mehtabsbassi2@users.noreply.github.com>
2025-09-21 00:12:54 +0300

    Update Navbar.jsx

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,25 +1,12 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { getProfile } from "../../redux/userSlice";
-import { useNavigate } from "react-router-dom";
-import { LOGIN_PAGE, MANAGER, PROFILE } from "../../util/Routes";
-import { FaChevronDown } from "react-icons/fa";
+
 
 const Navbar = () => {
   const profile = useSelector(getProfile);
-  const navigate = useNavigate();
   const [openMenu, setOpenMenu] = useState(false);
 
-  const handleProfile = () => {
-    navigate(PROFILE);
-    setOpenMenu(false);
-  };
-
-  const handleSwitchAccount = () => {
-  
-    setOpenMenu(false);
-    navigate(MANAGER)
-  };
 
   return (
     <div className="w-full h-[100px] px-6 flex items-center justify-end border-b border-ibmborder bg-white shadow-sm relative">
@@ -36,25 +23,8 @@ const Navbar = () => {
           alt="Profile"
           className="w-9 h-9 rounded-full object-cover border border-gray-300"
         />
-        <FaChevronDown className="text-gray-500 text-xs" />
       </div>
-
-      {openMenu && (
-        <div className="absolute top-[80px] right-6 w-48 bg-white border border-gray-200 shadow-lg rounded-lg overflow-hidden z-50">
-          <button
-            onClick={handleProfile}
-            className="w-full text-left px-4 py-2 text-sm hover:bg-gray-100"
-          >
-            View Profile
-          </button>
-          <button
-            onClick={handleSwitchAccount}
-            className="w-full text-left px-4 py-2 text-sm hover:bg-gray-100"
-          >
-            Switch to Manager
-          </button>
-        </div>
-      )}
+   
     </div>
   );
 };
