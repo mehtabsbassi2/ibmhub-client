ab7fe316d0ff99728607022cacf48222042bad5f
mehtabsbassi2 <148270493+mehtabsbassi2@users.noreply.github.com>
2025-09-21 17:22:08 +0300

    Update Navbar.jsx

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,18 +1,21 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { getProfile } from "../../redux/userSlice";
+import { useNavigate } from "react-router-dom";
+import { PROFILE } from "../../util/Routes";
 
 
 const Navbar = () => {
   const profile = useSelector(getProfile);
   const [openMenu, setOpenMenu] = useState(false);
+  const navigate = useNavigate()
 
 
   return (
     <div className="w-full h-[100px] px-6 flex items-center justify-end border-b border-ibmborder bg-white shadow-sm relative">
       {/* Profile Section with Dropdown */}
       <div
-        onClick={() => setOpenMenu(!openMenu)}
+        onClick={() =>navigate(PROFILE)}
         className="flex items-center gap-2 cursor-pointer"
       >
         <span className="text-sm font-medium hidden md:block text-gray-700">
